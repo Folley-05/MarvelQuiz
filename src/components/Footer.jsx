ba0370a76cbca13d46098aad90544e795a36d1b5
Folley-05 <aristotepascaldjounda@gmail.com>
2020-10-16 15:54:11 +0100

    modification de la taille de la police de la landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer>
+        <footer >
             <div className="footer-container">
-                <p>Projet realise par DonkeyGeek-2020</p>
+                <p style={{fontSize: 15}}>Un Projet realise par DonkeyGeek-2020</p>
                 <p>Les icones des personnages marvel sont prises sur iconFinder.com</p>
             </div>
         </footer>
